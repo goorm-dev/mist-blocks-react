ee286a8a3dea7e06141e1248f5d8669e8a6895bc
jason.lee <jason.lee@jasonleeui-MacBookPro.local>
2025-07-25 19:03:17 +0900

    tailwincss 픽스

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import HeroSection1 from './components/Hero_1/HeroSection1'
 import GridSection1 from './components/Grid_1/GridSection1';
 import AccordionFaq from './components/AccordionFaq/AccordionFaq';
 import FooterFull from './components/FooterFull/FooterFull';
+import Navbar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <main>
+        <Navbar />
         <HeroSection2 />
         <HeroSection1 />
         <GridSection1 />
