eaa0af02faf31118f162f9dfd168fd6bdf411759
rookieBaikGoorm <rookie.baik@goorm.io>
2025-08-21 15:16:41 +0900

    feat: ngrok 테스트 부팅을 위해 Vite 설정 일부 변경

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['f1bf1c04352f.ngrok-free.app'],
+  },
   plugins: [react(), tailwindcss()],
 })
